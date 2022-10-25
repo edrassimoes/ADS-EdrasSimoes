f55b88f29121fb6d7eea295035dfb16c3be79a61
Edras Simões <88869141+edrassimoes@users.noreply.github.com>
2022-10-25 08:48:04 -0300

    Add files via upload

---
+++ LABS_ADS/workspace_vscode/aluno.py
@@ -0,0 +1,15 @@
+class Aluno:
+    nome = "João"
+    nota = 45
+
+class AlunoGraduacao(Aluno):
+    nome = "José"
+    nota = 55
+
+# class AlunoPosGraduacao(Aluno, AlunoGraduacao):
+#     pass
+
+# aluno_1 = AlunoPosGraduacao
+
+# print(f"Nome: {aluno_1.nome}")
+# print(f"Nota: {aluno_1.nota}")
